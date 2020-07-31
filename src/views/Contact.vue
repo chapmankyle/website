f2cd19565667f80a1705b5efe72f729e402df288
Kyle Chapman <kyleichapman@gmail.com>
2020-07-31 23:27:55 +0200

    Add fields for user to give contact information and send a message

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,29 @@
 <template>
   <v-container>
     <h1>Contact Me</h1>
+
+    <v-row>
+      <v-col cols="12" sm="4" md="4">
+        Name
+      </v-col>
+      <v-col cols="12" sm="8" md="8">
+        <v-text-field flat dense dark></v-text-field>
+      </v-col>
+
+      <v-col cols="12" sm="4" md="4">
+        Email Address
+      </v-col>
+      <v-col cols="12" sm="8" md="8">
+        <v-text-field dense dark></v-text-field>
+      </v-col>
+
+      <v-col cols="12" sm="4" md="4">
+        Message
+      </v-col>
+      <v-col cols="12" sm="8" md="8">
+        <v-textarea dense dark></v-textarea>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -12,4 +35,8 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+h1, .row {
+  color: #e3e3e3 !important;
+}
+</style>
