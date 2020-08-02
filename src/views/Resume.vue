9eeb88e5eb667da8b2c77712200df7c3b0421838
Kyle Chapman <kyleichapman@gmail.com>
2020-08-02 23:50:37 +0200

    Add embedded resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,8 +1,9 @@
 <template>
   <v-container>
-    <h1 class="mx-6 py-5">
-      <mark class="heading-shadow">Resume</mark>
-    </h1>
+    <h1 class="mt-6">Resume</h1>
+    <v-divider class="mb-6"></v-divider>
+
+    <embed class="mb-10" src="resume.pdf" type="application/pdf" width="100%" height="1200px"/>
   </v-container>
 </template>
 
