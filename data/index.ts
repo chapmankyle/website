328d2139e1603952521a5cb216a7c6112ce61b88
Kyle Chapman <kyleichapman@gmail.com>
2024-10-22 22:20:33 +0200

    chore: use production API URL

--- data/index.ts
+++ data/index.ts
@@ -1,36 +1,13 @@
-/**
- * Base URL to use for the API endpoint.
- * TODO: Change this to the actual API URL.
- */
-const BASE_URL = 'http://localhost:8787/api/v2'
+import { APIData } from '@/lib/types'
 
-interface Experience {
-  id: number
-  dateAsString?: string
-  startDate: string
-  endDate: string
-  title: string
-  type: string
-  company: string
-  description: string
-  technologies: string[]
-}
+/** Base URL to use for the API endpoint */
+const BASE_URL = process.env.API_ROOT_URL || ''
 
-export interface APIData {
-  metadata: {
-    name: string
-    initials: string
-    url: string
-    location: string
-    title: string
-    summary: string
-    languages: string[]
-  }
-  experience: Experience[]
-}
+/** API endpoint URL */
+const API_URL = `${BASE_URL}/api/v2`
 
 export const fetchAPIData = async (): Promise<APIData> => {
-  const response = await fetch(`${BASE_URL}/all`)
+  const response = await fetch(`${API_URL}/all`)
   return (await response.json()).data
 }
 
