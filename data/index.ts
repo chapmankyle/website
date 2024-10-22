27271b1d3c9a6f819a29ce20cefb3548940ac4f9
Kyle Chapman <kyleichapman@gmail.com>
2024-10-22 23:03:43 +0200

    chore: export URLs from data file

--- data/index.ts
+++ data/index.ts
@@ -1,10 +1,10 @@
 import { APIData } from '@/lib/types'
 
 /** Base URL to use for the API endpoint */
-const BASE_URL = process.env.API_ROOT_URL ?? ''
+export const BASE_URL = process.env.API_ROOT_URL ?? ''
 
 /** API endpoint URL */
-const API_URL = `${BASE_URL}/api/v2`
+export const API_URL = `${BASE_URL}/api/v2`
 
 export const fetchAPIData = async (): Promise<APIData> => {
   const response = await fetch(`${API_URL}/all`)
