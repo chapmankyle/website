6b0a56523245a7cb183e93e6ab9a03e58bd72463
Kyle Chapman <kyleichapman@gmail.com>
2024-10-28 17:38:58 +0100

    feat: add Sentry for error logging

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -8,11 +8,11 @@ import Home from '@/components/home'
 
 import { FALLBACK_DATA } from '@/data/fallback'
 
-export default function Error ({
+export default function GlobalError ({
   error
 }: {
   error: Error & { digest?: string }
-}): JSX.Element {
+}) {
   const data = FALLBACK_DATA
 
   useEffect(() => {
